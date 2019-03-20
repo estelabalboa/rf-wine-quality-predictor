d36d17fb90912cce986f8e21757ec137785fb025
Estela Balboa <estela.balboa@gmail.com>
2019-03-20 22:51:41 +0100

    feat: Split de los datos en training y test, prediccion, import de las feature_importances

--- wine_prediction.py
+++ wine_prediction.py
@@ -1,10 +1,10 @@
 #  Import libraries necessary for this project
 import numpy as np
 import pandas as pd
-from time import time
-from IPython.display import display # Allows the use of display() for displaying DataFrames
+from IPython.display import display
 
 import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
 
 # Load the Red Wines dataset
 data = pd.read_csv("data/winequality-white.csv", sep=';')
@@ -68,4 +68,90 @@ plt.title('quality VS volatile acidity')
 plt.tight_layout()
 plt.show()
 plt.gcf().clear()
+
+
+quality_alcohol = data[['alcohol', 'quality']]
+g = sns.JointGrid(x="alcohol", y="quality", data=quality_alcohol, height=6)
+g = g.plot_joint(sns.regplot, scatter_kws={"s": 10})
+g = g.plot_marginals(sns.distplot)
+
+fig, axs = plt.subplots(ncols=1,figsize=(10,6))
+sns.barplot(x='quality', y='alcohol', data=quality_alcohol, ax=axs)
+plt.title('quality VS alcohol')
+
+plt.tight_layout()
 plt.show()
+plt.gcf().clear()
+# plt.show()
+
+# For each feature find the data points with extreme high or low values
+for feature in data.keys():
+    # TODO: Calculate Q1 (25th percentile of the data) for the given feature
+    Q1 = np.percentile(data[feature], q=25)
+
+    # TODO: Calculate Q3 (75th percentile of the data) for the given feature
+    Q3 = np.percentile(data[feature], q=75)
+
+    # TODO: Use the interquartile range to calculate an outlier step (1.5 times the interquartile range)
+    interquartile_range = Q3 - Q1
+    step = 1.5 * interquartile_range
+
+    # Display the outliers
+    print("Data points considered outliers for the feature '{}':".format(feature))
+    display(data[~((data[feature] >= Q1 - step) & (data[feature] <= Q3 + step))])
+
+# OPTIONAL: Select the indices for data points you wish to remove
+outliers = []
+
+# Remove the outliers, if any were specified
+good_data = data.drop(data.index[outliers]).reset_index(drop=True)
+
+#Displays the first 2 columns
+display(data.head(n=5))
+
+# Split the data into features and target label
+quality_raw = data['quality']
+features_raw = data.drop(['quality'], axis=1)
+
+# Import train_test_split
+
+# Split the 'features' and 'income' data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(features_raw,
+                                                    quality_raw,
+                                                    test_size=0.1,
+                                                    random_state=42)
+
+# Show the results of the split
+print("Training set has {} samples.".format(X_train.shape[0]))
+print("Testing set has {} samples.".format(X_test.shape[0]))
+
+# Import any three supervised learning classification models from sklearn
+from sklearn.ensemble import RandomForestRegressor
+#from sklearn.linear_model import LogisticRegression
+
+# Initialize the three models
+reg = RandomForestRegressor(min_samples_leaf=9, n_estimators=100)
+reg.fit(X_train, y_train)
+
+y_predict = reg.predict(X_test)
+
+from sklearn.metrics import r2_score
+sco = reg.score(X_test, y_test)
+R2 = r2_score(y_test, y_predict)
+print("R2 : ", R2)
+print("Score : ", sco)
+
+
+# Import a supervised learning model that has 'feature_importances_'
+model = RandomForestRegressor(min_samples_leaf=5, n_estimators=250)
+
+# Train the supervised model on the training set using .fit(X_train, y_train)
+model = model.fit(X_train, y_train)
+
+# Extract the feature importances using .feature_importances_
+importances = model.feature_importances_
+
+print(X_train.columns)
+print("Importances", importances)
+
+# TODO: plot as an histogram using fastai library
